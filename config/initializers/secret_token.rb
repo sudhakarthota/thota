600ef9dee6890953ff2c9e9c851b051025faa984
sudhakarthota <thota.sudhakar@gmail.com>
2014-01-31 13:34:17 +0530

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thota::Application.config.secret_key_base = '70107aa6da45f0a371266330c3cba4c1d759dccbc757bd730db1ffba28e39de101bafb5b9270725157b19b50f42fc2ceef2399c0c318fb8b5304f5ea71014ea2'
